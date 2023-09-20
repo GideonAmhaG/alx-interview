9bff0048ddd65b39f9492bb3d9598002bd7cc4a1
GideonAmhaG <gideonamha@gmail.com>
2023-09-20 11:41:05 +0300

    returned 0 instead of -1 for the 1st edgs case

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -20,7 +20,7 @@ def makeChange(coins, total):
     the aforementioned function
     """
     if total <= 0:
-        return -1
+        return 0
 
     dp = [total + 1] * (total + 1)
     dp[0] = 0
